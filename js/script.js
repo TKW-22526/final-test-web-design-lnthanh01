1f517ac9ed9c2fce823fe1a5c380779d7cc05fb7
Thanh <lenhatthanhcktv2006@gmail.com>
2026-05-25 15:41:23 +0700

    New update

--- js/script.js
+++ js/script.js
@@ -2,14 +2,15 @@
 const cars = [
 
     {
-        id: 1,
-        name: "KIA Carnival",
-        price: "900.000đ/ngày",
-        seats: "7 chỗ",
-        gearbox: "Tự động",
-        year: "2022",
-        image: "../assets/image/anh1.jpg",
-        desc: "Xe rộng rãi, option hiện đại, cửa lùa,..."
+    id: 1,
+    name: "KIA Carnival",
+    price: "900.000đ/ngày",
+    seats: "7 chỗ",
+    gearbox: "Tự động",
+    year: "2022",
+    image: "../assets/image/carnival/1.jpg",
+    folder: "carnival",
+    desc: "Xe rộng rãi, option hiện đại..."
     },
 
     {
@@ -19,29 +20,32 @@ const cars = [
         seats: "4 chỗ",
         gearbox: "Tự động",
         year: "2024",
-        image: "../assets/image/anh2.jpg",
+        image: "../assets/image/mazda3/1.jpg",
+        folder: "mazda3",
         desc: "Thiết kế thể thao, nội thất cao cấp."
     },
 
     {
         id: 3,
-        name: "Mazda 3 Sport Premium 2023",
+        name: "Hyundai Accent 1.4T",
         price: "600.000đ/ngày",
         seats: "4 chỗ",
         gearbox: "Tự động",
         year: "2023",
-        image: "../assets/image/anh3.jpg",
+        image: "../assets/image/accent/1.jpg",
+        folder: "accent",
         desc: "Sedan hiện đại, vận hành mạnh mẽ."
     },
 
       {
         id: 4,
-        name: "Hyundai Accent",
-        price: "600.000đ/ngày",
-        seats: "4 chỗ",
+        name: "Hyundai Custin 1.5T",
+        price: "1.000.000đ/ngày",
+        seats: "7 chỗ",
         gearbox: "Tự động",
         year: "2024",
-        image: "../assets/image/anh5.jpg",
+        image: "../assets/image/custin/1.jpg",
+        folder: "custin",
         desc: "Sedan hiện đại, nội thất cao cấp, form dáng trẻ trung."
     },
 
@@ -52,173 +56,69 @@ const cars = [
         seats: "7 chỗ",
         gearbox: "Tự động",
         year: "2026",
-        image: "../assets/image/anh4.jpg",
+        image: "../assets/image/mvp7/1.jpg",
+        folder : "mvp7",
         desc: "Xe điện 7 chổ, tiết kiệm chi phí, vận hành êm ái."
     },
 
       {
         id: 6,
-        name: "Hyundai Custin 1.5T",
+        name: "Toyota Innova Cross Hybrid 2024",
         price: "1.000.000đ/ngày",
         seats: "7 chỗ",
         gearbox: "Tự động",
         year: "2024",
-        image: "../assets/image/anh18.jpg",
-        desc: "Xe sang cửa lùa hiện đại, nội thất đẹp, khoang hành khách rộng rãi."
+        image: "../assets/image/innova/1.jpg",
+        folder: "innova",
+        desc: "Xe sang, hiện đại, nội thất đẹp, khoang hành khách rộng rãi."
     },
 
        {
         id: 7,
-        name: "Hyundai Accent",
+        name: "KIA K3 2024",
         price: "600.000đ/ngày",
         seats: "4 chỗ",
         gearbox: "Tự động",
         year: "2024",
-        image: "../assets/image/anh7.jpg",
+        image: "../assets/image/kiak3/1.jpg",
+        folder: "kiak3",
         desc: "Sedan hiện đại, nội thất cao cấp, form dáng trẻ trung."
     },
 
       {
         id: 8,
-        name: "Toyota Inova Cross",
+        name: "Mitsubishi XFORCE 2024",
         price: "900.000đ/ngày",
-        seats: "8 chỗ",
+        seats: "7 chỗ",
         gearbox: "Tự động",
-        year: "2024",
-        image: "../assets/image/anh6.jpg",
-        desc: "Vận hành mạnh mẽ, tiết kiệm nhiên liệu."
+        year: "2025",
+        image: "../assets/image/mitsu/1.jpg",
+        folder: "mitsu",
+        desc: "Vận hành mạnh mẽ, nội thất hiện đại."
     },
 
        {
         id: 9,
-        name: "Hyundai Accent",
-        price: "600.000đ/ngày",
-        seats: "4 chỗ",
-        gearbox: "Tự động",
+        name: "Hyundai Solati",
+        price: "2.000.000đ/ngày",
+        seats: "16 chỗ",
+        gearbox: "Số sàn",
         year: "2024",
-        image: "../assets/image/anh9.jpg",
+        image: "../assets/image/solati/1.jpg",
+        folder: "solati",
         desc: "Sedan hiện đại, nội thất cao cấp, form dáng trẻ trung."
     },
 
        {
         id: 10,
-        name: "Hyundai Accent",
-        price: "600.000đ/ngày",
-        seats: "4 chỗ",
-        gearbox: "Tự động",
-        year: "2024",
-        image: "../assets/image/anh10.jpg",
-        desc: "Sedan hiện đại, nội thất cao cấp, form dáng trẻ trung."
-    },
-
-       {
-        id: 11,
-        name: "Hyundai Accent",
-        price: "600.000đ/ngày",
-        seats: "4 chỗ",
-        gearbox: "Tự động",
-        year: "2024",
-        image: "../assets/image/anh11.jpg",
-        desc: "Sedan hiện đại, nội thất cao cấp, form dáng trẻ trung."
-    },
-
-      {
-        id: 12,
-        name: "Huyndai Solati",
-        price: "2.000.000đ/ngày",
-        seats: "16 chỗ",
-        gearbox: "Tự sàn",
-        year: "2024",
-        image: "../assets/image/anh8.jpg",
-        desc: "Nội thất sang trong, ghế bọc da nappa cao cấp, có hệ thống karaoke âm thanh chất lượng."
-    },
-
-       {
-        id: 13,
-        name: "Hyundai Accent",
-        price: "600.000đ/ngày",
-        seats: "4 chỗ",
-        gearbox: "Tự động",
-        year: "2024",
-        image: "../assets/image/anh14.jpg",
-        desc: "Sedan hiện đại, nội thất cao cấp, form dáng trẻ trung."
-    },
-
-      {
-        id: 14,
         name: "Vinfast Limogreen",
         price: "900.000đ/ngày",
         seats: "7 chỗ",
         gearbox: "Tự động",
-        year: "2025",
-        image: "../assets/image/anh12.jpg",
-        desc: "Xe điện 7 chổ, tiết kiệm chi phí, vận hành êm ái."
-    },
-
-      {
-        id: 15,
-        name: "Hyundai Accent",
-        price: "600.000đ/ngày",
-        seats: "4 chỗ",
-        gearbox: "Tự động",
         year: "2024",
-        image: "../assets/image/anh14.jpg",
-        desc: "Sedan hiện đại, nội thất cao cấp, form dáng trẻ trung."
-      },
-
-      {
-        id: 16,
-        name: "Mazda 3",
-        price: "600.000đ/ngày",
-        seats: "4 chỗ",
-        gearbox: "Tự động",
-        year: "2024",
-        image: "../assets/image/anh13.jpg",
-        desc: "Sedan form trẻ trung hiện đại, vận hành mạnh mẽ."
-    },
-
-      {
-        id: 17,
-        name: "KIA K3",
-        price: "600.000đ/ngày",
-        seats: "4 chỗ",
-        gearbox: "Tự động",
-        year: "2024",
-        image: "../assets/image/anh16.jpg",
-        desc: "Form xe thể thao, vận hành mạnh mẽ."
-    },
-
-      {
-        id: 18,
-        name: "Mitsubishi XFORCE Premium",
-        price: "1.200.000đ/ngày",
-        seats: "5 chỗ",
-        gearbox: "Tự động",
-        year: "2024",
-        image: "../assets/image/anh15.jpg",
-        desc: "SUV hiện đại, vận hành mạnh mẽ."
-    },
-
-      {
-        id: 19,
-        name: "KIA Sportage",
-        price: "1.200.000đ/ngày",
-        seats: "5 chỗ",
-        gearbox: "Tự động",
-        year: "2024",
-        image: "../assets/image/anh8.jpg",
-        desc: "SUV hiện đại, vận hành mạnh mẽ."
-    },
-
-      {
-        id: 20,
-        name: "KIA Sportage",
-        price: "1.200.000đ/ngày",
-        seats: "5 chỗ",
-        gearbox: "Tự động",
-        year: "2024",
-        image: "../assets/image/anh8.jpg",
-        desc: "SUV hiện đại, vận hành mạnh mẽ."
+        image: "../assets/image/limogreen/1.jpg",
+        folder: "limogreen",
+        desc: "Xe hiện đại, nội thất cao cấp, form dáng trẻ trung."
     },
 
 ];
@@ -265,7 +165,7 @@ function renderCars(data){
                         ${car.year}
                     </p>
 
-                    <a href="chi-tiet.html?id=${car.id}"
+                    href="chi-tiet.html?id=${car.id}&from=sanpham"
                        class="detail-btn">
 
                         Xem chi tiết
@@ -346,15 +246,8 @@ if(detail){
 
                 <div class="thumb-list">
 
-                    <img class="active-thumb"
-                    src="${car.image}"
-                    alt="">
-
-                    <img src="../assets/image/anh1.jpg" alt="">
-                    <img src="../assets/image/anh2.jpg" alt="">
-                    <img src="../assets/image/anh3.jpg" alt="">
-                    <img src="../assets/image/anh4.jpg" alt="">
-                    <img src="../assets/image/anh5.jpg" alt="">
+                    ${[1,2,3,4,5,6].map(num => `
+                    <img class="${num === 1 ? 'active-thumb' : ''}"src="../assets/image/${car.folder}/${num}.jpg"alt="">`).join('')}
 
                 </div>
 
